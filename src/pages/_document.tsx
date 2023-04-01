526f81c6dbeb644f2ac41d19bbcc9884da187cf5
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-04-01 22:32:47 +0300

    fix icons location

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,16 @@ export default function Document () {
     return (
         <Html lang="en">
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
                 <link
-                href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-                rel="stylesheet"/>
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&
+                    display=swap"
+                    rel="stylesheet"></link>
             </Head>
             <body>
-                <Main />
-                <NextScript />
+                <Main/>
+                <NextScript/>
             </body>
         </Html>
     )
