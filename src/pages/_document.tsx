187cca4ff016d4a3366c15011c81b251e5a3172a
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-04-01 23:31:14 +0300

    fix adding font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document () {
                 <link
                     rel="preconnect"
                     href="https://fonts.gstatic.com"
-                    crossOrigin="true"
+                    crossOrigin=""
                 />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900
