0254f34090d1d11706df2a4a0e3db50ce80ea5ad
EduardaNaida <eduarda.naida@gmail.com>
2023-04-02 15:20:36 +0300

    feat: add header

---
+++ src/pages/components/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from "app/styles/header.module.scss";
+
+export const Header = () => {
+  return (
+    <div className={style.header}><p className={style.headerText}>Inctagram</p></div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { Button, ButtonTheme } from 'shared/ui/Button/Button'
 import IconArrow from 'shared/assets/icons/general/arrow-back.svg'
+import {Header} from "pages/components/header";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function Home () {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
+              <Header/>
                 <Button theme={ButtonTheme.PRIMARY}>Text</Button>
                 <IconArrow/>
             </main>
