f2ee8a6899b37ab8239fc4bfcacde36d2f5de065
EduardaNaida <eduarda.naida@gmail.com>
2023-04-03 12:04:04 +0300

    feat: add input style for outline

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,15 +1,40 @@
+import React, {InputHTMLAttributes, ReactNode} from "react";
 import cls from './Input.module.scss'
 import clsx from 'clsx'
 
-interface InputProps {
-    className?: string
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  variant?: string
+  placeholder?: string
+  className?: string
+  disabled?: boolean
+  children?: ReactNode
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
 export const Input = (props: InputProps) => {
-    const { className } = props
-    return (
-        <div className={clsx(cls.Input, {}, [className])}>
+  const {
+    className,
+    children,
+    onChange,
+    disabled,
+    variant,
+    placeholder,
+    ...otherProps
+  } = props
 
-        </div>
-    )
+  const mods = {
+    [cls.disabled]: disabled
+  }
+
+  return (
+    <input
+      type={'text'}
+      className={clsx(cls.Input, mods, [className, cls[variant]])}
+      disabled={disabled}
+      onChange={onChange}
+      placeholder={placeholder}
+    >
+      {children}
+    </input>
+  )
 }
