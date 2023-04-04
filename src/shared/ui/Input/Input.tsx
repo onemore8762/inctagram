c5203fa796172adb0ea7221e972ff1213b6b00df
EduardaNaida <eduarda.naida@gmail.com>
2023-04-04 17:19:52 +0300

    fix: eslint

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,45 +1,45 @@
-import React, {InputHTMLAttributes, ReactNode} from "react";
+import React, { type InputHTMLAttributes, type ReactNode } from 'react'
 import cls from './Input.module.scss'
 import clsx from 'clsx'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  type?: string  /*text or password*/
-  variant?: string  /*outline or search*/
-  placeholder?: string
-  className?: string
-  disabled?: boolean
-  children?: ReactNode
-  onChange?: React.ChangeEventHandler<HTMLInputElement>
+    type?: string /* text or password */
+    variant?: string /* outline or search */
+    placeholder?: string
+    className?: string
+    disabled?: boolean
+    children?: ReactNode
+    onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
 export const Input = (props: InputProps) => {
-  const {
-    className,
-    children,
-    onChange,
-    disabled,
-    variant,
-    placeholder,
-    type,
-    ...otherProps
-  } = props
+    const {
+        className,
+        children,
+        onChange,
+        disabled,
+        variant = 'outline',
+        placeholder,
+        type,
+        ...otherProps
+    } = props
 
-  const mods = {
-    [cls.disabled]: disabled,
-  }
+    const mods = {
+        [cls.disabled]: disabled
+    }
 
-  const typeStyle = type === 'password' ? 'password' : ''
+    const typeStyle = type === 'password' ? 'password' : ''
 
-  return (
-    <input
+    return (
+        <input
       type={type}
       className={clsx(cls.Input, mods, [className, cls[variant], cls[typeStyle]])}
       disabled={disabled}
       onChange={onChange}
       placeholder={placeholder}
       {...otherProps}
-    >
-      {children}
-    </input>
-  )
+        >
+            {children}
+        </input>
+    )
 }
