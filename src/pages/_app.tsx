ebf173441cf2f11c5d5b81bff885a61ccdbfec99
onemore8762 <denis.churkin.afikei@mail.ru>
2023-04-04 23:37:40 +0300

    add: language in Congratulations and Verification, AdminMenu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../app/providers/ThemeProvider'
 import { appWithTranslation } from 'next-i18next'
 import { Layout } from 'shared/ui/Layout/Layout'
+import AdminMenu from '../shared/ui/AdminMenu/AdminMenu'
 
 function App ({ Component, pageProps }: AppProps) {
     return <ThemeProvider >
         <Layout>
+            <AdminMenu/>
             <Component {...pageProps} />
         </Layout>
     </ThemeProvider>
--- src/shared/ui/Input/Input.stories.tsx
+++ src/shared/ui/Input/Input.stories.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { type ComponentMeta, type ComponentStory } from '@storybook/react'
-
-import { Input } from './Input'
-
-export default {
-    title: 'shared/Input',
-    component: Input,
-    argTypes: {
-        backgroundColor: { control: 'color' }
-    }
-} as ComponentMeta<typeof Input>
-
-const Template: ComponentStory<typeof Input> = () => <Input/>
-
-export const Normal = Template.bind({})
-Normal.args = {}
+// import React from 'react'
+// import { type ComponentMeta, type ComponentStory } from '@storybook/react'
+//
+// import { Input } from './Input'
+//
+// export default {
+//     title: 'shared/Input',
+//     component: Input,
+//     argTypes: {
+//         backgroundColor: { control: 'color' }
+//     }
+// } as ComponentMeta<typeof Input>
+//
+// // const Template: ComponentStory<typeof Input> = () => <Input/>
+// //
+// // export const Normal = Template.bind({})
+// // Normal.args = {}
