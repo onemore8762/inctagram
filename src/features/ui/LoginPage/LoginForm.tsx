73f149667c1bcbc1325960cf4f18f016e46f391b
EduardaNaida <eduarda.naida@gmail.com>
2023-04-05 22:16:56 +0300

    feat: add useTranslation and text

--- src/features/ui/LoginPage/LoginForm.tsx
+++ src/features/ui/LoginPage/LoginForm.tsx
@@ -1,64 +1,64 @@
-import React, {useState} from 'react'
-import {Input} from '../../../shared/ui/Input/Input'
-import {FormWrapper} from '../../../shared/ui/FormWrapper/FormWrapper'
-import {Button} from '../../../shared/ui/Button/Button'
+import React, { useState } from 'react'
+import { Input } from '../../../shared/ui/Input/Input'
+import { FormWrapper } from '../../../shared/ui/FormWrapper/FormWrapper'
+import { Button } from '../../../shared/ui/Button/Button'
 import cls from './LoginForm.module.scss'
 import GoogleIcon from '../../../shared/assets/icons/general/google.svg'
 import FacebookIcon from '../../../shared/assets/icons/general/facebook.svg'
 
 interface LoginFormProps {
-  loginText: string
-  buttonRegister: string
-  text: string
-  forgotText: string
-  errorText: string
+    loginText: string
+    buttonRegister: string
+    text: string
+    forgotText: string
+    errorText: string
 }
 
-export default function LoginForm(props: LoginFormProps) {
-  const {
-    loginText,
-    buttonRegister,
-    text,
-    forgotText,
-    errorText
-  } = props
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+export default function LoginForm (props: LoginFormProps) {
+    const {
+        loginText,
+        buttonRegister,
+        text,
+        forgotText,
+        errorText
+    } = props
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const onSubmit = () => {
-    alert(email + '' + password)
-  }
+    const onSubmit = () => {
+        alert(email + '' + password)
+    }
 
-  return (
-    <FormWrapper className={cls.signInBlock}>
-      <div className={cls.signInText}><p>{loginText}</p></div>
-      <div className={cls.icons}>
-        <GoogleIcon/>
-        <FacebookIcon/>
-      </div>
+    return (
+        <FormWrapper className={cls.signInBlock}>
+            <div className={cls.signInText}><p>{loginText}</p></div>
+            <div className={cls.icons}>
+                <GoogleIcon/>
+                <FacebookIcon/>
+            </div>
 
-      <div className={cls.inputBlock}>
-        <Input type={'email'} className={cls.inputEmail}
-               onChange={event => {
-                 setEmail(event.currentTarget.value)
-               }}/>
-        <Input type={'password'}
-               errorText={errorText}
-               className={cls.inputPassword}
-               onChange={event => {
-                 setPassword(event.currentTarget.value)
-               }}/>
-      </div>
+            <div className={cls.inputBlock}>
+                <Input type={'email'} className={cls.inputEmail}
+                       onChange={event => {
+                           setEmail(event.currentTarget.value)
+                       }}/>
+                <Input type={'password'}
+                       errorText={errorText}
+                       className={cls.inputPassword}
+                       onChange={event => {
+                           setPassword(event.currentTarget.value)
+                       }}/>
+            </div>
 
-      <div className={cls.forgotPassword}><a href={'/'}>{forgotText}</a></div>
+            <div className={cls.forgotPassword}><a href={'/'}>{forgotText}</a></div>
 
-      <Button theme={'primary'} size={'medium'} block className={cls.button} onClick={onSubmit}>
-        {loginText}
-      </Button>
+            <Button theme={'primary'} size={'medium'} block className={cls.button} onClick={onSubmit}>
+                {loginText}
+            </Button>
 
-      <div className={cls.text}><p>{text}</p></div>
+            <div className={cls.text}><p>{text}</p></div>
 
-      <div className={cls.signUp}><a href="/signUp">{buttonRegister}</a></div>
-    </FormWrapper>
-  )
+            <div className={cls.signUp}><a href="/signUp">{buttonRegister}</a></div>
+        </FormWrapper>
+    )
 };
--- src/pages/[locale]/LoginPage.tsx
+++ src/pages/[locale]/LoginPage.tsx
@@ -1,10 +1,16 @@
-import LoginForm from "../../features/ui/LoginPage/LoginForm";
+import LoginForm from '../../features/ui/LoginPage/LoginForm'
+import { useTranslation } from 'next-i18next'
+import { makeStaticProps, getStaticPaths } from '../../shared/lib/getStatic'
 
-export default function LoginPage() {
-  return (<LoginForm loginText={'Sign In'}
-                     buttonRegister={'Sign Up'}
-                     text={'Don`t have an account?'}
-                     forgotText={'Forgot Password'}
-                     errorText={'The password or email you entered is incorrect.\n' +
-                       'Please try again'}/>)
+const getStaticProps = makeStaticProps(['common'])
+export { getStaticPaths, getStaticProps }
+
+export default function LoginPage () {
+    const { t } = useTranslation()
+    return (<LoginForm loginText={t('congratulations-button')} // Sign In
+                       buttonRegister={'Sign Up'}
+                       text={'Don`t have an account?'}
+                       forgotText={'Forgot Password'}
+                       errorText={'The password or email you entered is incorrect.\n' +
+    'Please try again'}/>)
 }
