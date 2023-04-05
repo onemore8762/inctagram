212d86489f4eb37ec6fd7385581fbb16ae8275ae
EduardaNaida <eduarda.naida@gmail.com>
2023-04-05 17:39:14 +0300

    fix: add useState for password and email

--- src/pages/[locale]/features/[signIn].tsx
+++ src/pages/[locale]/features/[signIn].tsx
@@ -7,32 +7,36 @@ import GoogleIcon from '../../../shared/assets/icons/general/google.svg'
 import FacebookIcon from '../../../shared/assets/icons/general/facebook.svg'
 
 export default function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = () => {
+    alert(email + password)
+  }
 
   return (
-      <FormWrapper className={cls.signInBlock}>
-        <div className={cls.signInText}><p>Sign In</p></div>
-        <div className={cls.icons}>
-          <GoogleIcon/>
-          <FacebookIcon/>
-        </div>
+    <FormWrapper className={cls.signInBlock}>
+      <div className={cls.signInText}><p>Sign In</p></div>
+      <div className={cls.icons}>
+        <GoogleIcon/>
+        <FacebookIcon/>
+      </div>
 
-        <div className={cls.inputBlock}>
-          <Input type={'email'}/>
-          <Input type={'password'} errorText={'The password or email you entered is incorrect.\n' +
-            'Please try again'}/>
-        </div>
+      <div className={cls.inputBlock}>
+        <Input type={'email'} className={cls.inputEmail} onChange={event => setEmail(event.currentTarget.value)}/>
+        <Input type={'password'} errorText={'The password or email you entered is incorrect.\n' +
+          'Please try again'} className={cls.inputPassword} onChange={event => setPassword(event.currentTarget.value)}/>
+      </div>
 
-        <div className={cls.forgotPassword}><a href={'/'}>Forgot Password</a></div>
+      <div className={cls.forgotPassword}><a href={'/'}>Forgot Password</a></div>
 
-        <div className={cls.button}>
-          <Button theme={'primary'} size={'medium'} block>
-            Sign In
-          </Button>
-        </div>
+      <Button theme={'primary'} size={'medium'} block className={cls.button} onClick={onSubmit}>
+        Sign In
+      </Button>
 
-        <div className={cls.text}><p>Don't have an account?</p></div>
+      <div className={cls.text}><p>Don't have an account?</p></div>
 
-        <div className={cls.signUp}><a href="">Sign Up</a></div>
-      </FormWrapper>
+      <div className={cls.signUp}><a href="/signUp">Sign Up</a></div>
+    </FormWrapper>
   );
 };
