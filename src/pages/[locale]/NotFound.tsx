68b2b734c835ece8443f133e7afbb7b97de03c59
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-04-09 12:44:32 +0300

    fix 404

--- src/pages/[locale]/NotFound.tsx
+++ src/pages/[locale]/NotFound.tsx
@@ -1,13 +1,13 @@
 import NotFoundImg from 'shared/assets/images/404error.png'
-import InfoPage from '../../shared/components/infoPage/InfoPage'
+import { Info } from '@/entities/Info'
 
 export default function NotFound () {
     return (
-        <InfoPage title={'404! Page not found'}
-                  text={'Looks like the page you are looking for is not available'}
-                  buttonText={'Back'}
-                  image={NotFoundImg}
-                  onClick={() => { history.back() }}
+        <Info title={'404! Page not found'}
+              text={'Looks like the page you are looking for is not available'}
+              buttonText={'Back'}
+              image={NotFoundImg}
+              onClick={() => { history.back() }}
         />
     )
 }
