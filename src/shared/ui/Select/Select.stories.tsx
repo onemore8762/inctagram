7e6a8543633a3bc4154f91b3a2ea461b20a1a342
Lyudmila Shchepina <lyudashchepina1999@gmail.com>
2023-04-09 17:20:01 +0300

    add select-box

---
+++ src/shared/ui/Select/Select.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { type ComponentMeta, type ComponentStory } from '@storybook/react'
+
+import { Select } from './Select'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
+import { Theme } from 'app/providers/ThemeProvider'
+
+export default {
+    title: 'shared/Select',
+    component: Select,
+    argTypes: {
+        backgroundColor: { control: 'color' }
+    },
+    args: {
+        options: ['Selected-box 1', 'Selected-box 2', 'Selected-box 3']
+    }
+} as ComponentMeta<typeof Select>
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args}/>
+export const Normal = Template.bind({})
+Normal.args = {
+}
+export const NormalDisabled = Template.bind({})
+NormalDisabled.args = {
+    disabled: true
+}
+export const NormalDark = Template.bind({})
+NormalDark.args = {
+}
+NormalDark.decorators = [ThemeDecorator(Theme.DARK)]
+export const NormalDarkDisabled = Template.bind({})
+NormalDarkDisabled.args = {
+    disabled: true
+}
+NormalDarkDisabled.decorators = [ThemeDecorator(Theme.DARK)]
---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,53 @@
+import React, { type SelectHTMLAttributes, useState } from 'react'
+import clsx from 'clsx'
+import cls from './Select.module.scss'
+import ArrowDown from 'shared/assets/icons/general/arrow-Down.svg'
+import ArrowUp from 'shared/assets/icons/general/arrow-Up.svg'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLInputElement> {
+    options: string[]
+    selectedItem?: string
+    disabled?: boolean
+}
+export const Select = (props: SelectProps) => {
+    const {
+        options,
+        selectedItem,
+        disabled,
+        ...otherProps
+    } = props
+    const [isOpen, setIsOpen] = useState(false)
+    const [selectedOption, setSelectedOption] = useState(selectedItem)
+
+    const toggling = () => { setIsOpen(!isOpen) }
+
+    const onOptionClicked = (value: string) => () => {
+        setSelectedOption(value)
+        setIsOpen(false)
+    }
+
+    const mods = {
+        [cls.active]: isOpen,
+        [cls.disabled]: disabled
+    }
+
+    return (
+        <div className={clsx(cls.DropDownContainer)}>
+            <button type='button' className={clsx(cls.DropDownHeader, mods)} disabled={disabled} onClick={toggling} >
+                {selectedOption || options[0]}
+                {isOpen ? <ArrowUp className={clsx(cls.Icon)} /> : <ArrowDown className={clsx(cls.Icon)} />}
+            </button>
+            {isOpen && (
+                <div>
+                    <div className={clsx(cls.DropDownList)}>
+                        {options.map(option => (
+                            <li className={clsx(cls.ListItem)} onClick={onOptionClicked(option)} key={Math.random()}>
+                                {option}
+                            </li>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
