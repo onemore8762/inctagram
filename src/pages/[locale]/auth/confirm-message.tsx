15fff0fbb83678d164d6bfeb8610241f8ffc0009
onemore8762 <denis.churkin.afikei@mail.ru>
2023-04-12 19:26:43 +0300

    fix: error

--- src/pages/[locale]/auth/confirm-message.tsx
+++
@@ -1,5 +0,0 @@
-import { ConfirmMessageForm } from '@/features/authorization/ui/confirmMessageForm/ConfirmMessageForm'
-
-export default function ConfirmMessage () {
-    return <ConfirmMessageForm/>
-}
