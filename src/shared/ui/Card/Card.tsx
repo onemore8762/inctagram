1e8c3e745c7b514bc79a98ca6bc53c2dec068d2f
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-04-27 17:47:13 +0300

    feat: card component

---
+++ src/shared/ui/Card/Card.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Image, { type ImageProps } from 'next/image'
+import clsx from 'clsx'
+import { Skeleton } from '../Skeleton/Skeleton'
+
+import cls from './Card.module.scss'
+
+type PropsType = ImageProps & {
+    cardWrapperClassName?: string
+    skeletonWidth?: string | number
+    skeletonHeight?: string | number
+    fallbackSrc?: string
+}
+
+export const Card: React.FC<PropsType> = (props) => {
+    const {
+        src,
+        alt,
+        width,
+        height,
+        sizes = '100vw',
+        skeletonWidth,
+        skeletonHeight = 'inherit',
+        cardWrapperClassName,
+        fallbackSrc
+    } = props
+    const [isLoaded, setIsLoaded] = useState(true)
+    const [onErrorSrc, setOnErrorSrc] = useState<string | undefined>(undefined)
+
+    const onImageLoadingComplete = () => {
+        setIsLoaded(false)
+    }
+
+    const handleOnError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        e?.currentTarget?.src !== fallbackSrc && setOnErrorSrc(fallbackSrc)
+    }
+
+    return (
+        <div className={clsx(cls.container, cardWrapperClassName)}>
+            {isLoaded && <Skeleton width={skeletonWidth} height={skeletonHeight} />}
+            <Image
+                    alt={alt}
+                    src={onErrorSrc || src}
+                    fill
+                    sizes={sizes}
+                    width={width}
+                    height={height}
+                    className={cls.image}
+                    onError={handleOnError}
+                    onLoadingComplete={onImageLoadingComplete}
+            />
+        </div>
+    )
+}
