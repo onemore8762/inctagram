b271c4d639cf4f98264f5e63596386741e9701d7
MarikaKonturova <mashakonturova@gmail.com>
2023-05-28 18:21:07 +0300

    add tabs & fix imageProfile

--- src/features/profile/components/ProfilePage/ProfilePage.tsx
+++ src/features/profile/components/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useGetProfileData } from 'app/hooks/useGetProfileData'
+import { AppRoutes } from 'shared/config/routeConfig/path'
+import { routerPush } from 'shared/lib/routerPush/routerPush'
 import { Avatar } from 'shared/ui/Avatar/Avatar'
+import { Button } from 'shared/ui/Button/Button'
 import { Card } from 'shared/ui/Card/Card'
 
 import cls from './ProfilePage.module.scss'
@@ -8,30 +11,37 @@ import cls from './ProfilePage.module.scss'
 export const ProfilePage = () => {
     const { response } = useGetProfileData()
     const userData = response?.data
+    const onSettingsRedirectClick = () => {
+        routerPush(AppRoutes.PROFILE.UPDATEPROFILE)
+    }
     return (
         <div className={cls.container}>
-            <div className={cls.flex}>
-                <div className={cls.avatar}>
-                    <Avatar size={192} src={userData ? userData.avatarUrl : undefined} />
-                </div>
-                <div>
-                    <div className={cls.userName}>{userData?.userName}</div>
-                    <div className={cls.info}>
-                        <div>
-                            <div className={cls.subscribe}>2 218</div>
-                            <div>Subscriptions</div>
-                        </div>
-                        <div>
-                            <div className={cls.subscribe}>2358</div>
-                            <div>Subscribers</div>
-                        </div>
-                        <div>
-                            <div className={cls.subscribe}>2764</div>
-                            <div>Publications</div>
-                        </div>
+            <div className={cls.infoButton}>
+                <div className={cls.flex}>
+                    <div className={cls.avatar}>
+                        <Avatar size={192} src={userData ? userData.avatarUrl.replace('FILES_URL=', '') : undefined} />
                     </div>
-                    <div className={cls.aboutMe}>{userData?.aboutMe}</div>
+                    <div>
+                        <div className={cls.userName}>{userData?.userName}</div>
+                        <div className={cls.info}>
+                            <div>
+                                <div className={cls.subscribe}>2 218</div>
+                                <div>Subscriptions</div>
+                            </div>
+                            <div>
+                                <div className={cls.subscribe}>2358</div>
+                                <div>Subscribers</div>
+                            </div>
+                            <div>
+                                <div className={cls.subscribe}>2764</div>
+                                <div>Publications</div>
+                            </div>
+                        </div>
+                        <div className={cls.aboutMe}>{userData?.aboutMe}</div>
+                    </div>
                 </div>
+                <Button onClick={onSettingsRedirectClick} className={cls.button} > Profile Settings</Button>
+
             </div>
             <div className={cls.cardsList}>
                 <div className={cls.card}>
