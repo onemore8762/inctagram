94c3bf649b9388bbf7e1104b4264db2ca6cbf515
Zhanat Lepesbayeva <zhanat.lepesbaeva@effective-soft.com>
2023-06-03 22:15:16 +0300

    fix: include missed code

--- src/shared/api/post/myPostService.ts
+++ src/shared/api/post/myPostService.ts
@@ -1,6 +1,3 @@
-import { type AxiosError, type AxiosResponse } from 'axios'
-import { comment } from 'postcss'
-
 import { $api } from 'shared/api/api'
 import { type Post } from '../../types/post'
 
